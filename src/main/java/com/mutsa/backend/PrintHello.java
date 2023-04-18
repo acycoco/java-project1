215e6d73ba12eda2ade9c4bf8ff76d51ffd8efac
LUCKYCOM\space <pink10257@naver.com>
2023-04-18 15:31:04 +0900

    first commit

---
+++ src/main/java/com/mutsa/backend/PrintHello.java
@@ -0,0 +1,7 @@
+package com.mutsa.backend;
+
+public class PrintHello { //클래스
+    public void print() { //매소드
+        System.out.println(1 + 1); //명령(command)
+    }
+}
---
+++ src/main/java/com/mutsa/backend/PrintHelloTest.java
@@ -0,0 +1,8 @@
+package com.mutsa.backend;
+
+public class PrintHelloTest {
+    public static void main(String[] args) {
+        PrintHello printHello = new PrintHello();
+        printHello.print();
+    }
+}
